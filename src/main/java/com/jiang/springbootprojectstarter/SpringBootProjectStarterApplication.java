b8e0cd9dcbacff28ffbd9ab7cb83cdb378849b28
Yuming Jiang <cjcjiang@163.com>
2018-05-22 14:22:33 +0800

    Project init.

---
+++ src/main/java/com/jiang/springbootprojectstarter/SpringBootProjectStarterApplication.java
@@ -0,0 +1,12 @@
+package com.jiang.springbootprojectstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectStarterApplication.class, args);
+	}
+}
